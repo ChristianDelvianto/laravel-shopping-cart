db15292e90bd02882e2fa0e5693bce077b21eeb6
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2025-12-31 04:32:32 +0700

    feat(products): add cartItems relationship when fetching Product detail

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,11 +18,21 @@ class ProductController extends Controller
         ]);
     }
 
-    public function show(Product $product)
+    public function show(Request $request, Product $product)
     {
+        $currentUser = $request->user();
+
         if ($product->status !== 'active') {
             abort(404, 'Product not available');
         }
+
+        $product->load([
+            'cartItems' => function ($query) use ($currentUser) {
+                $query->whereHas('cart', function ($q) use ($currentUser) {
+                    $q->where('user_id', $currentUser->id);
+                });
+            }
+        ]);
 
         // Show other products for user (using custom algorithm)
         $recommended = Product::where('id', '!=', $product->id)
