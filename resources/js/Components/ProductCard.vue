57331b2e8b97693c4f215021fa54bfaa8aa38dac
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2025-12-31 04:06:31 +0700

    feat(products): add ProductCard component

---
+++ resources/js/Components/ProductCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    product: { type: Object, required: true }
+});
+</script>
+
+<template>
+    <Link
+        :href="route('products.show', product.id)"
+        class="border border-stone-300 flex flex-col overflow-hidden product-card rounded-lg w-full"
+    >
+        <span class="product-image-placeholder"></span>
+
+        <span class="product-info">
+            <span class="block break-words whitespace-normal w-full">{{ product.name }}</span>
+
+            <span class="font-semibold text-blue-600 text-lg">${{ product.price }}</span>
+        </span>
+    </Link>
+</template>
