9d0b970590398c0ab117216a76d1ff0e8a68818f
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-01-03 03:58:33 +0700

    feat(cron): add daily sales report and clean up product notification schedule

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('send-daily-sales-report')->dailyAt('01:00');
+Schedule::command('clean-up-product-notify-records')->dailyAt('01:15');
