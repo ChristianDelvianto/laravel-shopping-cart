6f5b981996eb31365e829f5fa2ebfda5a36988d8
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-01-02 16:08:01 +0700

    feat(order): add orders page

---
+++ resources/js/Pages/Orders/Index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, usePage } from '@inertiajs/vue3';
+
+const page = usePage();
+</script>
+
+<template>
+    <Head title="My order" />
+
+    <AuthenticatedLayout>
+        <div class="py-8">
+            
+        </div>
+    </AuthenticatedLayout>
+</template>
