fd9cc0a1b3ab5a58f0df05527aac5d652449f5dd
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-01-02 15:59:33 +0700

    feat(order): add OrderSeeder functionality

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +16,44 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $products = Product::where('stock_quantity', '>', 0)
+                           ->where('status', 'active')
+                           ->get();
+
+        User::all()->each(function ($user) use ($products) {
+            $ordersCount = rand(0, 3);
+            
+            $orders = Order::factory($ordersCount)->create([
+                'user_id' => $user->id,
+                'status' => 'completed',
+            ]);
+
+            foreach ($orders as $order) {
+                $orderProducts = $products->random(rand(1, min(5, $products->count())));
+
+                $subtotal = 0;
+
+                foreach ($orderProducts as $product) {
+                    $quantity = rand(1, min(5, $product->stock_quantity));
+
+                    $totalPrice = $product->price * $quantity;
+
+                    OrderItem::factory()->create([
+                        'order_id' => $order->id,
+                        'product_id' => $product->id,
+                        'quantity' => $quantity,
+                        'unit_name' => $product->name,
+                        'unit_price' => $product->price,
+                        'total_price' => $totalPrice
+                    ]);
+
+                    $product->decrement('stock_quantity', $quantity);
+
+                    $subtotal += $totalPrice;
+                }
+
+                $order->update(['subtotal_amount' => $subtotal]);
+            }
+        });
     }
 }
