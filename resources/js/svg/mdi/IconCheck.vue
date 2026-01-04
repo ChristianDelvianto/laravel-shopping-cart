bed7ad358c232fc9aecb1322eb7339401d7c0e9c
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-01-04 10:42:38 +0700

    feat(layout): add IconCheck icon

---
+++ resources/js/svg/mdi/IconCheck.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+    color: {
+        type: String,
+        default: 'rgb(82, 82, 82)'
+    },
+    size: {
+        type: Number,
+        default: 24
+    }
+});
+</script>
+
+<template>
+    <svg
+        :height="size"
+        :width="size"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            :fill="color"
+            d="M21 7L9 19l-5.5-5.5l1.41-1.41L9 16.17L19.59 5.59z"
+        />
+    </svg>
+</template>
